723c140a2133fad0059fda15113734d937676815
Anton718 <anton.kostroma@yahoo.com>
2024-02-26 07:35:58 -0500

    add update

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getInfo(): string {
-    return 'This is a nest-js app';
+    return 'This is a nest-js app at https://github.com/Anton718/nestjs-authentication1';
   }
 }
